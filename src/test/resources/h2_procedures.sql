21a15b4c352d1359c83c011e24b9cb24cef8b89b
Philippe Marschall <philippe.marschall@gmail.com>
2016-09-27 20:30:09 +0200

    - fix H2 tests - fix Derby tests

--- src/test/resources/h2_procedures.sql
+++ src/test/resources/h2_procedures.sql
@@ -1,3 +1,3 @@
-CREATE ALIAS STRING_PROCEDURE FOR "com.github.marschall.storedprocedureprox.H2ProcedureSources.stringProcedure";
+CREATE ALIAS STRING_PROCEDURE FOR "com.github.marschall.storedprocedureproxy.H2ProcedureSources.stringProcedure";
 
 CREATE ALIAS VOID_PROCEDURE FOR "com.github.marschall.storedprocedureproxy.H2ProcedureSources.voidProcedure";
