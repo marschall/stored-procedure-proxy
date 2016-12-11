3fab635602e3fee97865274e98c7d60ab411f030
Philippe Marschall <philippe.marschall@gmail.com>
2016-12-11 17:16:26 +0100

    better coverage

--- src/test/java/com/github/marschall/storedprocedureproxy/spi/NamingStrategyTest.java
+++ src/test/java/com/github/marschall/storedprocedureproxy/spi/NamingStrategyTest.java
@@ -28,6 +28,22 @@ public class NamingStrategyTest {
   }
 
   @Test
+  public void thenCapitalize() {
+    NamingStrategy strategy = NamingStrategy.prefix("sp_")
+            .thenCapitalize();
+    assertEquals("Sp_blitz", strategy.translateToDatabase("blitz"));
+
+  }
+
+  @Test
+  public void capitalize() {
+    NamingStrategy strategy = NamingStrategy.capitalize();
+    assertEquals("Java", strategy.translateToDatabase("java"));
+    assertEquals("", strategy.translateToDatabase(""));
+    assertEquals("1", strategy.translateToDatabase("1"));
+  }
+
+  @Test
   public void withoutFirst() {
     NamingStrategy strategy = NamingStrategy.withoutFirst(2);
     assertEquals("javaName", strategy.translateToDatabase("x_javaName"));
@@ -39,6 +55,14 @@ public class NamingStrategyTest {
             .thenSnakeCase()
             .thenLowerCase();
     assertEquals("java_name", strategy.translateToDatabase("x_javaName"));
+  }
+
+  @Test
+  public void moreCompound() {
+    NamingStrategy strategy = NamingStrategy.snakeCase()
+            .thenUpperCase()
+            .thenWithoutFirst(2);
+    assertEquals("JAVA_NAME", strategy.translateToDatabase("xJavaName"));
   }
 
 }
