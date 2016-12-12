a2fef706a68d71cf942fe17cff3d202e5a0f5970
Philippe Marschall <philippe.marschall@gmail.com>
2016-12-12 20:52:16 +0100

    fix javadoc references

--- src/main/java/com/github/marschall/storedprocedureproxy/spi/TypeMapper.java
+++ src/main/java/com/github/marschall/storedprocedureproxy/spi/TypeMapper.java
@@ -67,10 +67,10 @@ import com.github.marschall.storedprocedureproxy.annotations.ReturnValue;
  * <tr><td>boolean</td><td>{@link Types#BOOLEAN}</td></tr>
 
  * <tr><td colspan="2">ARRAY</td></tr>
- * <tr><td>Collection</td><td>{@link Types#Array}</td></tr>
- * <tr><td>List</td><td>{@link Types#Array}</td></tr>
- * <tr><td>Set</td><td>{@link Types#Array}</td></tr>
- * <tr><td>array</td><td>{@link Types#Array}</td></tr>
+ * <tr><td>Collection</td><td>{@link Types#ARRAY}</td></tr>
+ * <tr><td>List</td><td>{@link Types#ARRAY}</td></tr>
+ * <tr><td>Set</td><td>{@link Types#ARRAY}</td></tr>
+ * <tr><td>array</td><td>{@link Types#ARRAY}</td></tr>
  * </table>
  */
 @FunctionalInterface
