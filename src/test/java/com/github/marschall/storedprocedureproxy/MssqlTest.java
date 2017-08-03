e1fb5883c2b6789047f288517dd975b9c8088892
Philippe Marschall <philippe.marschall@gmail.com>
2017-08-03 11:42:21 +0200

    MSSQL does not support client side cursors

--- src/test/java/com/github/marschall/storedprocedureproxy/MssqlTest.java
+++ src/test/java/com/github/marschall/storedprocedureproxy/MssqlTest.java
@@ -7,12 +7,6 @@ import static com.github.marschall.storedprocedureproxy.ProcedureCallerFactory.P
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeTrue;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Types;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -73,39 +67,6 @@ public class MssqlTest extends AbstractDataSourceTest {
   @Ignore
   public void fakeCursor() {
     assertEquals(Arrays.asList("hello", "world"), this.procedures.fakeCursor());
-  }
-
-  @Test
-  @Ignore
-  public void cursorOutParameter() throws SQLException {
-    try (Connection connection = this.getDataSource().getConnection();
-         Statement statement = connection.createStatement()) {
-      statement.execute("DROP PROCEDURE IF EXISTS simpleCursor");
-      statement.execute("CREATE PROCEDURE simpleCursor\n"
-+ "    @OutputCursor CURSOR VARYING OUTPUT\n"
-+ "AS\n"
-+ "BEGIN\n"
-+ "    SET @OutputCursor = CURSOR\n"
-+ "    FORWARD_ONLY STATIC FOR\n"
-+ "    SELECT 'hello' UNION ALL SELECT 'world';\n"
-+ "\n"
-+ "    OPEN @OutputCursor;\n"
-+ "END");
-      try (CallableStatement call = connection.prepareCall("{ call simpleCursor(?)}")) {
-        call.registerOutParameter(1, Types.REF_CURSOR);
-        call.execute();
-        try (ResultSet resultSet = call.getObject(1, ResultSet.class)) {
-          while (resultSet.next()) {
-            System.out.println(resultSet.getObject(1, String.class));
-          }
-        }
-      }
-//      List<String> values = new ArrayList<>(2);
-//      while (resultSet.next()) {
-//        values.add(resultSet.getString(1));
-//      }
-//      assertEquals(Arrays.asList("hello", "world"), values);
-    }
   }
 
 }
