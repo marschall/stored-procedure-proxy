0ab82795b7306adfd0b6b578d792965fc008077b
Philippe Marschall <philippe.marschall@gmail.com>
2017-10-01 17:00:05 -0700

    Add a module-info
    Closes #59

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.github.marschall.stored-procedure-proxy {
+
+  exports com.github.marschall.storedprocedureproxy;
+  exports com.github.marschall.storedprocedureproxy.annotations;
+  exports com.github.marschall.storedprocedureproxy.spi;
+
+  requires java.sql;
+
+  requires static spring.core;
+  requires static spring.jdbc;
+  requires static spring.tx;
+
+}
