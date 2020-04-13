be3bef7572ab58305306f6ded73742edd3fd5728
Philippe Marschall <philippe.marschall@gmail.com>
2020-04-13 17:28:04 +0200

    disable mariadb

--- src/test/java/com/github/marschall/storedprocedureproxy/configuration/MysqlConfiguration.java
+++ src/test/java/com/github/marschall/storedprocedureproxy/configuration/MysqlConfiguration.java
@@ -1,13 +1,13 @@
 package com.github.marschall.storedprocedureproxy.configuration;
 
+import static com.github.marschall.storedprocedureproxy.Travis.isTravis;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-
-import com.github.marschall.storedprocedureproxy.Travis;
 
 @Configuration
 public class MysqlConfiguration {
@@ -24,9 +24,9 @@ public class MysqlConfiguration {
     String userName = System.getProperty("user.name");
     String database = userName;
     // https://dev.mysql.com/doc/connector-j/6.0/en/connector-j-reference-configuration-properties.html
-    dataSource.setUrl("jdbc:mysql://localhost:3306/" + database + "?useSSL=false&allowPublicKeyRetrieval=true&logger=com.mysql.cj.log.Slf4JLogger");
+    dataSource.setUrl("jdbc:mysql://localhost:3306/" + database + "?useSSL=false&allowPublicKeyRetrieval=true&logger=com.mysql.cj.log.Slf4JLogger&disableMariaDbDriver=true");
     dataSource.setUsername(userName);
-    String password = Travis.isTravis() ? "" : userName;
+    String password = isTravis() ? "" : userName;
     dataSource.setPassword(password);
     return dataSource;
   }
