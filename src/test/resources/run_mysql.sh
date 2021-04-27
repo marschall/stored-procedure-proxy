5668a41824799d39207a2d5cc688f1956edd5119
Philippe Marschall <philippe.marschall@gmail.com>
2021-04-27 18:38:23 +0200

    bump database docker images

--- src/test/resources/run_mysql.sh
+++ src/test/resources/run_mysql.sh
@@ -10,5 +10,5 @@ docker run --name jdbc-mysql \
  -e MYSQL_DATABASE=$USER \
  -p 3306:3306 \
  --mount type=tmpfs,destination=/var/lib/mysql \
- -d mysql:8.0.21 \
+ -d mysql:8.0.24 \
  --log-bin-trust-function-creators=1
